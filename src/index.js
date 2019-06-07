c2cfc374b0c82501c4bee885b15c988e6440c9fc
Zubair <zubair.maqsood6@gmail.com>
2019-06-07 15:24:55 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,58 @@ let addToy = false
 
 // YOUR CODE HERE
 
+const addToyForm = document.querySelector(".add-toy-form")
+const toyNameInput = document.querySelector("input[name = 'name']");
+const toyImageInput = document.querySelector("input[name = 'image']")
+const toyCollection = document.querySelector("#toy-collection")
+const toyURL = `http://localhost:3000/toys`
+
+
+//This function fetches all toys
+function getToys() {
+  return fetch(toyURL)
+  .then((response) => response.json())
+}
+
+
+function addToys(toys){
+  toys.forEach((toy) => addToyCard(toy))
+
+}
+
+function addToyCard(toy) {
+  const tr = document.createElement('tr')
+   tr.innerHTML = `<div class="card">
+                              <h2>${toy.name}</h2>
+                              <img src = ${toy.image} class="toy-avatar" />
+                              <p>${toy.likes} Likes </p>
+                              <button class="like-btn">Like <3</button>
+                            </div>`
+
+  toyCollection.append(tr);
+
+
+}
+
+getToys().then((toys) => addToys(toys))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
