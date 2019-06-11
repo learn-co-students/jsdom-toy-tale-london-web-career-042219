2b2c9866197b48144cdfaff7baedfd645691dac6
Ross Mawdsley <rossmawd@gmail.com>
2019-06-11 16:43:38 +0100

    trying to get rid of blank cards

--- src/index.js
+++ src/index.js
@@ -1,19 +1,70 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+const addBtn = document.querySelector("#new-toy-btn");
+const createBtn = document.querySelector("input[name= 'submit']");
+let toyName = document.querySelector("input[name= 'name']");
+let toyImg = document.querySelector("input[name= 'image']");
+const toyForm = document.querySelector(".container"); //DO I need?
+const form = document.querySelector(".add-toy-form"); //tested
+let addToy = false;
 
-// YOUR CODE HERE
+let toyCollection = document.querySelector("#toy-collection");
 
-addBtn.addEventListener('click', () => {
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  getToys();
+});
+
+addBtn.addEventListener("click", () => {
   // hide & seek with the form
-  addToy = !addToy
+  addToy = !addToy;
   if (addToy) {
-    toyForm.style.display = 'block'
+    toyForm.style.display = "block";
     // submit listener here
   } else {
-    toyForm.style.display = 'none'
+    toyForm.style.display = "none";
   }
-})
+});
 
+// MY CODE:
+createBtn.addEventListener("submit", addNewToy(toyName.value, toyImg.value));
 
-// OR HERE!
+function addNewToy(toyName, toyImage) {
+  fetch(" http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name: toyName,
+      image: toyImage,
+      likes: 0
+    })
+  });
+  //then ...CATCH()!?
+}
+
+function getToys() {
+  //returns a Promise with the data
+  toyCollection.innerHTML = "";
+
+  return fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(object => {
+      console.log(object);
+
+      object.forEach(toy => {
+        console.log(toy.name);
+        //make one of these for each toy
+        toyCollection.innerHTML += `<div class="card">
+        <h2>${toy.name}</h2>
+        <img src=${toy.image} class="toy-avatar" />
+        <p>${toy.likes} </p>
+        <button class="like-btn">Like <3</button>
+      </div>`;
+      });
+    });
+}
+
+function doStuff(things) {
+  //what now?
+}
