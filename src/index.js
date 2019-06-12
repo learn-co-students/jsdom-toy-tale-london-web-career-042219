52bf598fe954cb091f8554c67e16136f6e05d729
Ross Mawdsley <rossmawd@gmail.com>
2019-06-12 09:40:02 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ let toyImg = document.querySelector("input[name= 'image']");
 const toyForm = document.querySelector(".container"); //DO I need?
 const form = document.querySelector(".add-toy-form"); //tested
 let addToy = false;
+const BASE_URL = `http://localhost:3000/toys/`;
 
 let toyCollection = document.querySelector("#toy-collection");
 
@@ -13,58 +14,90 @@ document.addEventListener("DOMContentLoaded", function() {
   getToys();
 });
 
+function getToys() {
+  //returns a Promise with the data
+  toyCollection.innerHTML = "";
+  return fetch(BASE_URL)
+    .then(resp => resp.json())
+    .then(object => {
+      object.forEach(toy => displayToyCard(toy));
+    });
+}
+
+function displayToyCard(toy) {
+  let toyDiv = document.createElement("div");
+  toyDiv.className = "card";
+  let labelName = document.createElement("h2");
+  let toyPic = document.createElement("img");
+  let pTagLikes = document.createElement("p");
+  let likeButton = document.createElement("button");
+
+  labelName.innerText = toy.name;
+  toyPic.src = toy.image;
+  toyPic.className = "toy-avatar"; //not adding an attribute>!
+  pTagLikes.innerText = toy.likes;
+  pTagLikes.dataset.toyId = toy.id;
+  likeButton.innerText = "Like <3";
+  likeButton.className = "like-btn";
+
+  toyDiv.append(labelName, toyPic, pTagLikes, likeButton);
+  toyCollection.append(toyDiv);
+
+  addLikeListener(toy, likeButton);
+}
+
+function addLikeListener(toy, likeButton) {
+  likeButton.addEventListener("click", () => {
+    event.preventDefault();
+    addLike(toy);
+    //debugger;
+  });
+}
+
+function addLike(toy) {
+  toy.likes += 1;
+
+  fetch(BASE_URL + toy.id, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      likes: toy.likes
+    })
+  });
+  let like = document.querySelector(`p[data-toy-id = '${toy.id}']`);
+  like.innerText = toy.likes;
+  //getToys();
+}
+
 addBtn.addEventListener("click", () => {
   // hide & seek with the form
   addToy = !addToy;
   if (addToy) {
     toyForm.style.display = "block";
-    // submit listener here
+    // submit listener here //WHHHYYYYY?
   } else {
     toyForm.style.display = "none";
   }
 });
-
 // MY CODE:
-createBtn.addEventListener("submit", addNewToy(toyName.value, toyImg.value));
+form.addEventListener("submit", addNewToy);
 
-function addNewToy(toyName, toyImage) {
-  fetch(" http://localhost:3000/toys", {
+function addNewToy() {
+  fetch(BASE_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json"
     },
     body: JSON.stringify({
-      name: toyName,
-      image: toyImage,
+      name: toyName.value,
+      image: toyImg.value,
       likes: 0
     })
-  });
+  }); //.catch(error => console.error("Ahh! It's all gone Wonks!")); //not working
+  //throw new Error("Network response was not ok."); // not working
   //then ...CATCH()!?
-}
-
-function getToys() {
-  //returns a Promise with the data
-  toyCollection.innerHTML = "";
-
-  return fetch("http://localhost:3000/toys")
-    .then(resp => resp.json())
-    .then(object => {
-      console.log(object);
-
-      object.forEach(toy => {
-        console.log(toy.name);
-        //make one of these for each toy
-        toyCollection.innerHTML += `<div class="card">
-        <h2>${toy.name}</h2>
-        <img src=${toy.image} class="toy-avatar" />
-        <p>${toy.likes} </p>
-        <button class="like-btn">Like <3</button>
-      </div>`;
-      });
-    });
-}
-
-function doStuff(things) {
-  //what now?
 }
